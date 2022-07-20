28d5a025b69385cd6a07218eea497afaeeb9d0c9
Fel-Cas <andres.cmonsalve@gmail.com>
2022-07-20 11:24:39 -0500

    Feat:Información de la aplicación en el endpoint de inicio

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  getHello() {
-    return {message:'Está '}
+  description() {
+    return {
+      university:'Universidad de Antioquia',
+      message:`Este proyecto se realiza con el fin de crear servicios para que sean utilizados por la aplicación biblioApp, ya que con el cambio de software utilizado en la biblioteca  no se tendrían estos servicios que son indispensables para el funcionamiento de la aplicación. `}
   }
 }
