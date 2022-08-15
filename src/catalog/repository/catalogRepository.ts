a3a5d376b7d6fdac7b447c7d3a409871259063e1
Fel-Cas <andres.cmonsalve@gmail.com>
2022-08-14 21:18:15 -0500

    Feat: creación de la query para buscar libro por isbn

--- src/catalog/repository/catalogRepository.ts
+++ src/catalog/repository/catalogRepository.ts
@@ -14,4 +14,10 @@ export class CatalogRepository {
         inner join biblioitems bitm on bi.biblionumber=bitm.biblioitemnumber
         where bi.title like '%${book}%' or bi.author like '%${book}%';   `);
     }
+
+    async getBookByISBN(isbn:String){
+        return await this.connection.query(`select title as titulo, author as autor, abstract as resumen, bitm.editionstatement  as edicion,i.barcode, i.onloan as fecha_prestamo  from biblio bi inner join biblioitems bitm on bi.biblionumber=bitm.biblionumber 
+        inner join items i on i.biblioitemnumber=bitm.biblioitemnumber
+        where bitm.isbn=${isbn}; `);
+    }
 }
